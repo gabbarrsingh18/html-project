b8a2db8a6473fb32d390209f8c04d0272a8863e3
gabbarrsingh18 <kushwahasuraj31736@gmail.com>
2026-09-20 15:16:32 +0530

    create Mini Project/Todo app⭐/app.js

---
+++ Mini Project/Todo app⭐/app.js
@@ -0,0 +1,41 @@
+let inp= document.querySelector("input");
+let butt= document.querySelector("button");
+let ul= document.querySelector("ul");
+
+butt.addEventListener("click",function(){
+  let item=document.createElement("li");
+  item.innerText=inp.value;
+  
+  let delBtn=document.createElement("button")
+  delBtn.innerText="✖️";
+  delBtn.classList.add(".delete");
+  
+  item.appendChild(delBtn);
+  
+  ul.appendChild(item);
+  inp.value="";
+  
+})
+
+
+
+ul.addEventListener("click", function(event){
+//  console.dir(event);
+  if(event.target.nodeName=="BUTTON"){
+    let listItem=event.target.parentElement;
+    listItem.remove();
+    console.log("deleted");
+  }
+});
+
+
+
+/*let delBtns=document.querySelectorAll(".delete");
+for(delBtn of delBtns){
+  delBtn.addEventListener("click",function(){
+ //   console.log("elements are deleted")
+    let par= this.parentElement;
+    console.log(par);
+    par.romove();
+  });*/
+  
